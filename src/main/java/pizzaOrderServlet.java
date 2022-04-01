29e606cb11ce1dad45d8ba673e2a6a0389b90fde
Charles Willcockson <willcockson.charles@gmail.com>
2022-04-01 15:05:46 -0500

    pizz servlet

--- src/main/java/pizzaOrderServlet.java
+++ src/main/java/pizzaOrderServlet.java
@@ -1,13 +1,27 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "pizzaOrderServlet", urlPatterns = "/pizzaorder")
+@WebServlet("/pizzaorder")
 public class pizzaOrderServlet extends HttpServlet{
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/pizzaform.jsp").forward(request, response);
+    }
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        System.out.println(request.getParameter("sauce"));
+        System.out.println(request.getParameter("size"));
+        System.out.println(request.getParameter("crust"));
+        String[] toppings = request.getParameterValues("toppings");
+        for(String topping : toppings){
+            System.out.print(topping);
+            System.out.print(" ");
+        }
+        System.out.println(" ");
+        System.out.println(request.getParameter("address"));
     }
 }
