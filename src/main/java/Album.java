d719a9ee9b2dcff7002dc67549b610fcbec32854
Charles Willcockson <willcockson.charles@gmail.com>
2022-04-04 10:28:00 -0500

    Album bean

---
+++ src/main/java/Album.java
@@ -0,0 +1,65 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Album implements Serializable {
+    private Integer id;
+    private String artist;
+    private String name;
+    private Date releaseDate;
+    private float sales;
+    private String genre;
+
+    public Album(){ };
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public float getSales() {
+        return sales;
+    }
+
+    public void setSales(float sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+
+    }
+
+    public void setReleaseDate(int i, int i1, int i2) {
+    }
+}
